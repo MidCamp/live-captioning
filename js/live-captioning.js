34c864d3f7ed1a34acd603c85eeea85e5293466c
Glenn Blicharz <glenn.blicharz@hs2solutions.com>
2019-04-25 09:16:57 -0500

    local-storage New variables to hold start/stop times for each webkitSpeechRecognition transcript entry Transcript text and times are stored once a transcript entry becomes finalized Helper functions to format times and output the transcript as an SRT file Function to store transcript output into localStorage

--- js/live-captioning.js
+++ js/live-captioning.js
@@ -5,11 +5,17 @@ var button = document.getElementById('lc-button');
 var recognizing = false;
 
 var recognition = new webkitSpeechRecognition();
+var transcript = [];
+var startTime;
+var beginTime;
+
 recognition.continuous = true;
 recognition.interimResults = true;
-  
+
 recognition.onstart = function() {
   recognizing = true;
+  startTime = new Date();
+  beginTime = startTime;
 };
 
 recognition.onerror = function(event) {};
@@ -21,15 +27,19 @@ recognition.onresult = function(event) {
   for (var i = event.resultIndex; i < event.results.length; ++i) {
     if(event.results[i][0].confidence > 0.4) {
       lc.firstChild.data = capitalize(event.results[i][0].transcript);
+      if (event.results[i].isFinal) {
+        addToTranscript(event.results[i][0].transcript);
+      }
     }
   }
 };
 
+
 function capitalize(s) {
   var first_char = /\S/;
- return s.replace(first_char, function(m) { 
-    return m.toUpperCase(); 
-  }); 
+  return s.replace(first_char, function(m) {
+    return m.toUpperCase();
+  });
 }
 
 function toggleSpeechRecognition(event) {
@@ -44,5 +54,62 @@ function toggleSpeechRecognition(event) {
     button.style.display = "none";
     $('body').toggleClass('lc-on');
     recognition.start();
+  }
+}
+
+/**
+ * Add text to the running transcript array, setting start/end times
+ *
+ * @param text - the text to be added to the transcript
+ */
+function addToTranscript(text){
+  if (text){
+    var endTime = new Date();
+    transcript.push({"startTime": beginTime, "endTime": endTime, "text": text});
+    // Reset the beginTime
+    beginTime = endTime;
+    saveToLocalStorage(formatTranscript());
+  }
+}
+
+
+function formatTime(timeString){
+  var time = new Date(timeString);
+  var elapsedTime = time - startTime;
+
+  var seconds = Math.floor(elapsedTime/1000);
+  var milliseconds = elapsedTime - (seconds * 1000);
+  var minutes = Math.floor(seconds/60);
+  var hours = Math.floor(minutes/60);
+  var days = Math.floor(hours/24);
+
+  hours = hours-(days*24);
+  minutes = minutes-(days*24*60)-(hours*60);
+  seconds = seconds-(days*24*60*60)-(hours*60*60)-(minutes*60);
+  return ((hours < 10) ? '0' : '') + hours + ((minutes < 10) ? ':0' : ':') + minutes  + ((seconds < 10) ? ':0' : ':') + seconds + "," + milliseconds;
+}
+
+/**
+ * Format the transcript array into an "SRT" format
+ *
+ */
+function formatTranscript(){
+  var output = "";
+  for (var i = 0; i < transcript.length; ++i) {
+    output += i+1 + "\n";
+    output += formatTime(transcript[i].startTime) + " --> " + formatTime(transcript[i].endTime) + "\n";
+    output += transcript[i].text + "\n\n";
+  }
+  return output;
+}
+
+/**
+ * Save the data to localStorage
+ *
+ * @param data - the data to save
+ */
+function saveToLocalStorage(data) {
+  if (data && localStorage) {
+    localStorage.setItem("live-captioning", data);
   }
 }
