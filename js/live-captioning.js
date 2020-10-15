604fbf9bb772f8910021d0ea1072d02dac01f062
Jacob Dozier <jacob.dozier@bounteous.com>
2020-10-15 16:52:28 -0500

    Adding markup for infusion preferences panel and styling tweaks.

--- js/live-captioning.js
+++ js/live-captioning.js
@@ -170,7 +170,7 @@ function viewSession(format) {
       break;
   }
   if (format){
-    $('#lc-transcript').show();    
+    $('#lc-transcript').show();
   } else {
     $('#lc-transcript').toggle();
   }
